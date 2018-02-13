3239b041749cc62f53229c6c0592fc919b8a8571
Unknown <grant.owens.boomer@gmail.com>
2018-02-12 20:33:53 -0800

    changes ModelFactory to use classes

--- core/schema/ModelFactory.js
+++ core/schema/ModelFactory.js
@@ -6,7 +6,15 @@ const mongoose = require("mongoose");
  * 
  * Creates
 */
-module.exports = {
+module.exports = class ModelFactory{
+    /** 
+     * ModelFactory
+     * 
+     * Factory object used to create new models for MongoDB
+     * 
+     * Creates
+    */
+    constructor(){}
     /**
      * Creates a new model. As a result of ```mongoose.model```, adds that 
      * model to mongooses internal registry
@@ -14,7 +22,7 @@ module.exports = {
      * @param{string} name - the name of the model. The name must be singular (i.e. Car, not Cars)
      * @param{obecjt} the schema params for the model. http://mongoosejs.com/docs/guide.html
      */
-    create : (name, schemaParams) => {
+    static create(name, schemaParams){
         let schema = new mongoose.Schema(schemaParams)
         return mongoose.model(name, schema);
     }
