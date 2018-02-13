aed722ae17c85352af46647d7eb70765c679df79
Unknown <grant.owens.boomer@gmail.com>
2018-02-12 20:45:16 -0800

    yes...yes! The power!

--- api/routes/characterRoute.js
+++ api/routes/characterRoute.js
@@ -1,13 +1,27 @@
 //looks like it is setup this way to take a dependency
+let mongoose = require('mongoose');
+let dndCharacter = require('../models/DungeonsAndDragons/DnDCharacter.model');
+let srCharacter = require('../models/Shadowrun/ShadowrunCharacter');
+let BasicCollectionController = require("../../core/api/BasicCollectionController")
 module.exports = (app) => {
-    var characterList = require('../controllers/characterController');
+    let dndCharacterController = new BasicCollectionController(dndCharacter);
+    let srCharacterController = new BasicCollectionController(srCharacter);
 
-    app.route('/characters')
-        .get(characterList.all)
-        .post(characterList.create);
+    app.route('/dnd/characters')
+        .get(dndCharacterController.all)
+        .post(dndCharacterController.create);
 
-    app.route('/characters/:characterId')
-        .get(characterList.getSingle)
-        .put(characterList.update)
-        .delete(characterList.delete);
+    app.route('/dnd/characters/:id')
+        .get(dndCharacterController.getSingle)
+        .put(dndCharacterController.update)
+        .delete(dndCharacterController.delete);
+
+    app.route('/sr/characters')
+        .get(srCharacterController.all)
+        .post(srCharacterController.create);
+
+    app.route('/sr/characters/:id')
+        .get(srCharacterController.getSingle)
+        .put(srCharacterController.update)
+        .delete(srCharacterController.delete);
 };
